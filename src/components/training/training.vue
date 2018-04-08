55cc8bc5c0f96b12a300090ed1865f9e3d7405df
shaikan@mail.ru <Muzma7411328>
2018-04-08 10:18:33 +0300

    trainning

--- src/components/training/training.vue
+++ src/components/training/training.vue
@@ -1,15 +1,4 @@
 <template>
-
-    <!--<section>-->
-        <!--<v-container grid-list-xl>-->
-            <!--<v-layout row wrap justify-center>-->
-                <!--<v-flex xs12 sm4 lg8>-->
-
-                <!--</v-flex>-->
-            <!--</v-layout>-->
-        <!--</v-container>-->
-    <!--</section>-->
-
         <v-tabs
                 dark
                 color="cyan"
@@ -34,22 +23,21 @@
                     <section>
                             <v-layout row wrap justify-center>
                                 <v-flex xs12 sm10 lg8 offset-xs0>
-                                    <img :src="training.img" :alt="training.img"
-                                         style="max-width: 100%;
-                                            max-height: 400px;
-                                            display: block;
-                                            margin-left: auto;
-                                            margin-right: auto;">
+                                    <!--<img :src="training.img" :alt="training.img"-->
+                                         <!--style="max-width: 100%;-->
+                                            <!--max-height: 400px;-->
+                                            <!--display: block;-->
+                                            <!--margin-left: auto;-->
+                                            <!--margin-right: auto;">-->
                                     <v-card class="elevation-0 transparent">
-                                    <!--<v-card-media :src="training.img" height="400px">-->
-                                    <!--</v-card-media>-->
+                                    <v-card-media :src="training.img" height="400px" contain>
+                                    </v-card-media>
                                     <v-card-title primary-title class="layout justify-center">
                                     <div class="headline text-xs-center"><h4>{{ training.name }}</h4></div>
                                     </v-card-title>
-                                    <v-card-text>{{ training.description }}</v-card-text>
-                                    <div>
-
-                                    </div>
+                                    <p class="word-wrap">{{ training.description }}</p>
+                                    <p class="word-wrap"><b>Что надо с собой иметь на тренировке: </b>{{ training.requirements }}</p>
+                                    <p class="word-wrap"><b>Длительность тренировки один час, включает в себя: </b>{{ training.duration }}</p>
                                     </v-card>
                                 </v-flex>
                             </v-layout>
@@ -73,10 +61,10 @@
                   {
                       'name' : 'Pole Intro',
                       'description' : 'Pole Intro - Тренировка для тех, кто пришел на пилон впервые. Это вводный уровень занятий, где мы с вами будем изучать несложные крутки, базовые хваты и зацепы. Длится данное направление от 3х до 8ми занятий. За это время вы успеваете "почувствовать" себя на пилоне и с определенной базой перейти к начинающему уровню.\n' +
-                      'Систематизированная методика обучения упрощает и даёт большую результативность ваших тренировок. Идея ввести такой уровень в обиход принадлежит известной спортсменке акробатики и спорта на пилоне Кристине Думанской. Мы гордимся и рады, что имеем возможность обучать вас по данной методике, приносящей прекрасные результаты.\n' +
-                      'Длительность тренировки один час, включает в себя: разминку, работу на пилоне, офп и заминку.\n' +
-                      'Что надо с собой иметь на тренировке: топ и шорты, носки (чешки или босиком), лосины (спортивные штаны) и кофта для разминки.',
-                      'img' : 'static/img/home/pole_sport.jpg'
+                      'Систематизированная методика обучения упрощает и даёт большую результативность ваших тренировок. Идея ввести такой уровень в обиход принадлежит известной спортсменке акробатики и спорта на пилоне Кристине Думанской. Мы гордимся и рады, что имеем возможность обучать вас по данной методике, приносящей прекрасные результаты.\n',
+                      'img' : 'static/img/home/pole_sport.jpg',
+                      'requirements' : 'топ и шорты, носки (чешки или босиком), лосины (спортивные штаны) и кофта для разминки.',
+                      'duration' : 'разминку, работу на пилоне, офп и заминку.'
                   },
                   {
                       'name' : 'Pole Kids',
@@ -95,5 +83,8 @@
 </script>
 
 <style scoped>
-
+    .word-wrap {
+        white-space: pre-wrap !important;
+        padding: 5px
+    }
 </style>
