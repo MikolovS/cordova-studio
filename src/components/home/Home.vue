1378f63827b7f995dfc750490b33d87e98e10181
shaikan@mail.ru <Muzma7411328>
2018-04-09 15:56:03 +0300

    command

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -118,10 +118,10 @@
             <v-flex xs12 sm4 offset-sm1>
               <v-card class="elevation-0 transparent">
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
+                  <div class="headline">Контактная Информация</div>
                 </v-card-title>
                 <div>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  По всем вопросам обращатся к руководителю студии - <b>Миколова Анастасия Юриевна</b>
                 </div>
                 <v-list class="transparent">
                   <v-list-tile>
@@ -137,7 +137,7 @@
                       <v-icon class="blue--text text--lighten-2">place</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                      <v-list-tile-title>Chicago, US</v-list-tile-title>
+                      <v-list-tile-title>Одесса, ул-Тираспольская 4</v-list-tile-title>
                     </v-list-tile-content>
                   </v-list-tile>
                   <v-list-tile>
